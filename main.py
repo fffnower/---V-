6cebc10bd13cf13e77f29fd2098c73b5a8ed4a1c
fffnower <32289652+fffnower@users.noreply.github.com>
2024-05-13 23:05:47 +0800

    Update and rename mian.py to main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pyautogui
 import time
 
-text = '这是一个测试。 this is a test.' # input()
+# 改为input()方法
+text = '这是一个测试。 this is a test.' 
 
-# 把焦点调整到输入框
+# 给个时间把焦点调整到输入框
 time.sleep(3)
 
+# 以中文方式开始
 chinese = True
+
 for char in text:
     unicode_chars = char.encode('unicode-escape').decode()
-    print(char, unicode_chars)
-
-    if chinese != True:
-        pyautogui.press('shift')
-        chinese = not chinese 
+    # print(char, unicode_chars)
+    # unicode_chars = '\uxxxx'
 
     if unicode_chars[:2] == r'\u':
         for code in f'vuc{unicode_chars[2:]}':
@@ -23,3 +23,7 @@ for char in text:
         pyautogui.press('shift')
         chinese = not chinese 
         pyautogui.typewrite(char)
+
+    if chinese != True:
+        pyautogui.press('shift')
+        chinese = not chinese 
