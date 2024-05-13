43b4e7fbf9b62c3815e70bdbff45e82c4e317dbc
fffnower <32289652+fffnower@users.noreply.github.com>
2024-05-13 23:28:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import pyautogui
 import time
 
-# 改为input()方法
-text = '这是一个测试。 this is a test.' 
-
 # 给个时间把焦点调整到输入框
-time.sleep(3)
+times = 5
 
-# 以中文方式开始
-chinese = True
+while True:
+    
+    print('使用微软IME中文输入模式，设置SHIFT为中英文切换；')
+    text = input(f'输入文字，并在{times}秒内切换的需粘贴的焦点位置：' )
+    time.sleep(times)
 
-for char in text:
-    unicode_chars = char.encode('unicode-escape').decode()
-    # print(char, unicode_chars)
-    # unicode_chars = '\uxxxx'
+    # 以中文方式开始
+    chinese = True
 
-    if unicode_chars[:2] == r'\u':
-        for code in f'vuc{unicode_chars[2:]}':
-            pyautogui.press(code)
-        pyautogui.press(' ')
-    else:
-        pyautogui.press('shift')
-        chinese = not chinese 
-        pyautogui.typewrite(char)
+    for char in text:
+        unicode_chars = char.encode('unicode-escape').decode()
+        # unicode_chars = '\uxxxx'
 
-    if chinese != True:
-        pyautogui.press('shift')
-        chinese = not chinese 
+        if unicode_chars[:2] == r'\u':
+            for code in f'vuc{unicode_chars[2:]}':
+                pyautogui.press(code)
+            pyautogui.press(' ')
+        else:
+            pyautogui.press('shift')
+            chinese = not chinese 
+            pyautogui.typewrite(char)
+
+        if chinese != True:
+            pyautogui.press('shift')
+            chinese = not chinese 
+
+    print('-----------------------------------------------')
